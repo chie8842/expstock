efa3dac736583d66f52c810e38807b5bb685ceca
chie8842 <chie-hayashida@cookpad.com>
2018-04-11 22:17:43 +0900

    enhance post_stock

--- expstock/expstock.py
+++ expstock/expstock.py
@@ -208,7 +208,7 @@ git_head: {}
         sys.stdout = open(stdout_path, 'w')
         sys.stderr = open(stderr_path, 'w')
 
-    def post_stock(self, result):
+    def post_stock(self, result=''):
         self.result = result
         sys.stdout.close()
         sys.stdout = sys.__stdout__
